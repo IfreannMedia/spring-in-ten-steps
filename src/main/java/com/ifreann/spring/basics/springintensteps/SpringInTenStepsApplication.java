f35cdb3e680da317e62d71221c9ae807df839fbf
Wario <37343942+IfreannMedia@users.noreply.github.com>
2021-10-23 12:42:16 +0200

    Initial commit

---
+++ src/main/java/com/ifreann/spring/basics/springintensteps/SpringInTenStepsApplication.java
@@ -0,0 +1,13 @@
+package com.ifreann.spring.basics.springintensteps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInTenStepsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInTenStepsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifreann/spring/basics/springintensteps/SpringInTenStepsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifreann.spring.basics.springintensteps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInTenStepsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
